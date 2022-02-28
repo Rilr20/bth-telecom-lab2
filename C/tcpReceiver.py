a7cd290eb906ca49ab10dfa466e86188d12a3bc9
Rilr20 <nrrl99@gmail.com>
2022-02-28 20:15:47 +0100

    stuff done

---
+++ C/tcpReceiver.py
@@ -0,0 +1,47 @@
+# Rikard Larsson Rilr20 BTH
+#kolla paketnummret
+#är det för högt? 10005 => 10007
+#kommer de i rätt ordning? 10006 => 10005
+#det ska loggas för att se om det är i ordning eller inte
+
+from socket import *
+serverPort = 12000
+
+# create TCP welcoming socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+
+# server starts listening for incoming TCP requests
+serverSocket.listen(1)
+
+print ('The TCP server is ready to receive')
+
+connectionSocket, addr = serverSocket.accept()
+prev_package = 0
+
+while True:
+    # server waits for incoming requests; new socket created on return
+    # read sentence of bytes from socket sent by the client
+    sentence = connectionSocket.recv(2048).decode()
+    split_sentence = sentence.split(";")
+    # print package number and sentence
+    if not sentence:
+        print("now i close")
+        connectionSocket.close()
+        break
+    # print (f'paketnummer {split_sentence[0]} {split_sentence[1][:-4]}')
+    if prev_package+1 == int(split_sentence[0]):
+        print(f'correct package arrived: {split_sentence[0]}')
+    elif prev_package < int(split_sentence[0]):
+        print(f'wrong package arrived too large expected {prev_package+1} got {split_sentence[0]}')
+    elif prev_package > int(split_sentence[0]):
+        # print(split_sentence[0])
+        # print(prev_package)
+        print(f'wrong package arrived too small expected {prev_package+1} got {split_sentence[0]}')
+
+    prev_package += 1
+    # print()
+    # if int(split_sentence[0]) >= 10:
+    #     print('I close :(')
+    #     connectionSocket.close()
+    #     break
---
+++ C/tcpSender.py
@@ -0,0 +1,67 @@
+# Rikard Larsson Rilr20 BTH
+
+#50ms asvstånd
+#kunna skjustera tiden mellan utskicken
+#skicka paket i 10sekunder
+#mindre än 1500bytes stora
+#inkludera ett meddelande nummer
+#10001;
+#10002;
+#avsluta med fyra ####
+
+from socket import *
+from datetime import datetime
+import time
+
+#destinations porten
+serverName = '192.168.1.31'
+serverPort = 12000
+#changes the speed of messages is sent
+time_to_end = 10
+waittime = 0.05
+
+waittime = input("Put in your waittime default is 0.05 seconds: ")
+if waittime == ""  or waittime.isalpha():
+    print("using default")
+    waittime = 0.05
+
+time_to_end = input("how many seconds should program run: ")
+if time_to_end == "" or time_to_end.isalpha():
+    print("using default 10 seconds")
+    time_to_end = 10
+
+# waittime = 0.05
+#converts to integer
+# waittime = int(waittime)
+#skapar ett meddelande som är 1400 byteslångt
+packet_size = 1400
+message = ""
+for i in range(0, int(packet_size/2)):
+    message += "ä"
+# create TCP socket on client to use for connecting to remote server
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# open the TCP connection
+clientSocket.connect((serverName, serverPort))
+
+# Input sentence from keyboard
+# sentence = input('Input lowercase sentence: ')
+
+# ska köra i 10 sekunder sedan sluta
+i = 0
+# this_list = [1,2,3,4,5,6,7,8,9,10,11,13,12,14,15,16,17,18,17,19]
+start_time = datetime.now()
+# while i < len(this_list):
+while True:
+    i += 1
+    # package = f'{this_list[i]};{message}####'
+    package = f'{i};{message}####'
+    clientSocket.send(package.encode())
+    print(f'package {i}')
+    time_delta = datetime.now() - start_time
+    time.sleep(float(waittime))
+    if time_delta.total_seconds() >= time_to_end:
+        break
+
+# close the TCP connection
+clientSocket.close()
